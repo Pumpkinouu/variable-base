55648232e275d404e476ebaa4694638a674f0da9
Pumpkinouu <zivanovicdanilo0@gmail.com>
2023-01-31 22:58:59 +0100

    Added the whole project

---
+++ 02a_VariablesDanilo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02a_VariablesDanilo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "les Variables en c#";
+            //Commamnde pour le titre de la console5
+
+            Console.WriteLine("╔════════════════════════════════════════════╗");
+            Console.WriteLine("║Introduction aux variables et typage dans c#║");
+            Console.WriteLine("╚════════════════════════════════════════════╝");
+
+
+            
+            Console.Write("Merci de me donner un nombre : ");
+            string Nombre = Console.ReadLine();
+            Console.SetCursorPosition(0, 6);
+            Console.WriteLine("Vous avez choisi : {0}",Nombre);
+            Console.ReadLine();
+            
+        }
+    }
+}
